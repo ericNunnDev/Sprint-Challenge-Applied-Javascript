48dd7cc4b6274ef5bc0eedc26fcb1f63c7eb6528
lowlyHero <e.nunn1009@gmail.com>
2019-11-01 18:59:41 -0400

    Fixed title and temp not dipslaying

--- components/Header/index.js
+++ components/Header/index.js
@@ -16,23 +16,25 @@ headerContainer.appendChild(Header());
 function Header() {
     // Define Elements
     const header = document.createElement('div');
-    const dateSpan = document.createElement('span');
     const title = document.createElement('h1');
+    const dateSpan = document.createElement('span');
     const tempSpan = document.createElement('span');
-
-    // Setup Structure
-    header.appendChild(dateSpan, title, tempSpan);
 
     // Set Class Names
     header.classList.add('header');
     dateSpan.classList.add('date');
     tempSpan.classList.add('temp');
-
+    
     // Set Text Content
-
+    
     dateSpan.textContent = 'SMARCH 28, 2019';
     title.textContent = 'Lambda Times';
     tempSpan.textContent = '98°';
+    
+    // Setup Structure
+    header.appendChild(dateSpan);
+    header.appendChild(title);
+    header.appendChild(tempSpan);
 
     return header;
 }
